df9471a687451360ad1198ea3038fb4ed9b94020
Gaetano Martello <gf.martello@gmail.com>
2023-07-28 13:52:05 -0400

    added footer and made minor homepage changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Element from './components/Element/Element';
 import Modality from './components/Modality/Modality';
 import MyCard from './pages/MyCard/MyCard';
 import Consultations from './pages/Consultations/Consultations';
+import Footer from './components/Footer/Footer';
 
 function App() {
   function InfoPage() {
@@ -30,6 +31,7 @@ function App() {
           <Route path='/mycard' element={<MyCard />} />
           <Route path='/consultations' element={<Consultations />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <a href='https://www.threads.net/@tarot_strology'>
+        <img className='footer__logo' src='https://seeklogo.com/images/T/threads-logo-E9BA734BF6-seeklogo.com.png?v=638252100960000000' alt='threads logo' />
+      </a>
+      <a href='https://www.instagram.com/tarot_strology/'>
+        <img className='footer__logo' src='https://freelogopng.com/images/all_img/1658586823instagram-logo-transparent.png' alt='instagram logo' />
+      </a>
+      <a href='mailto:tarotstrology.consultations@gmail.com'>
+        <img className='footer__logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png' alt='gmail logo'/>
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
