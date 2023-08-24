b7c874ded8a978356930cd5962f9070f1b102dcb
Gaetano Martello <gf.martello@gmail.com>
2023-08-24 09:27:28 -0400

    changed email

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
       <a href='https://www.instagram.com/gf.martello/' target="_blank">
         <img className='footer__logo' src='https://freelogopng.com/images/all_img/1658586823instagram-logo-transparent.png' alt='instagram logo' />
       </a>
-      <a href='mailto:tarotstrology.consultations@gmail.com'>
+      <a href='mailto:gf.martello@gmail.com'>
         <img className='footer__logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png' alt='gmail logo'/>
       </a>
     </footer>
